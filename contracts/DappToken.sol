997b863685fb7ae246354dd9960742f55d38ead9
Arya-xxx <63159692+Arya-xxx@users.noreply.github.com>
2021-06-01 21:28:28 +0530

    Token_sale

---
+++ contracts/DappToken.sol
@@ -0,0 +1,43 @@
+pragma solidity ^0.5.16;
+
+contract DappToken {
+
+string public name ="DApp Token";
+string public symbol ="DAPP";
+string public standard="DApp Token v1.0";
+
+
+event Transfer(
+    address indexed _from,
+    address indexed _to,
+    uint256 _value
+);
+
+
+
+uint256 public totalSupply;
+mapping(address=>uint256) public balanceOf;
+
+constructor(uint256 _initialSupply) public
+{
+    balanceOf[msg.sender]=_initialSupply;
+    totalSupply=_initialSupply;
+
+
+}
+
+function transfer(address _to, uint256 _value) public returns(bool success)
+{
+  require(balanceOf[msg.sender] >= _value);
+
+balanceOf[msg.sender]-=_value;
+balanceOf[_to]+=_value;
+emit Transfer(msg.sender,_to, _value);
+
+  return true;
+
+}
+
+
+
+}
